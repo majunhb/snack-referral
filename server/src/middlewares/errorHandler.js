d25c00594a98c52e6dab0836a0ab388e8ac7324d
majunhb <264765519@qq.com>
2026-06-15 18:24:36 +0800

    feat: add server/src/middlewares/errorHandler.js

---
+++ server/src/middlewares/errorHandler.js
@@ -0,0 +1,9 @@
+const logger = require('../utils/logger');
+
+module.exports = function errorHandler(err, req, res, next) {
+  logger.error(`${req.method} ${req.path} - ${err.message}`, { stack: err.stack });
+  if (err.code && err.code < 10000) {
+    return res.status(200).json({ code: err.code, msg: err.message, data: null });
+  }
+  res.status(500).json({ code: 500, msg: '服务器内部错误' });
+};
