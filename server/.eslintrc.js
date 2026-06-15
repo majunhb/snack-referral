54b418e23c8d9f6fb8d4d11b529ecb7ae57f8da7
majunhb <264765519@qq.com>
2026-06-15 19:01:43 +0800

    feat: add CI/CD pipeline (lint, test, docker build, mini-program check)

---
+++ server/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    jest: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'script'
+  },
+  rules: {
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_|next|req|res' }],
+    'no-console': 'off',
+    'no-undef': 'error',
+    'no-extra-semi': 'warn',
+    'semi': ['warn', 'always'],
+    'quotes': ['warn', 'single', { avoidEscape: true }],
+    'indent': ['warn', 2, { SwitchCase: 1 }],
+    'comma-dangle': ['warn', 'never'],
+    'eol-last': ['warn', 'always'],
+    'no-trailing-spaces': 'warn',
+    'no-multiple-empty-lines': ['warn', { max: 1 }],
+    'prefer-const': 'warn'
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'coverage/',
+    'dist/',
+    '*.min.js'
+  ]
+};
