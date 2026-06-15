09ae7c60ab3a994ea3b1355d8fa85b516f89214b
majunhb <264765519@qq.com>
2026-06-15 18:23:41 +0800

    feat: add miniprogram-merchant/pages/users/users.js

---
+++ miniprogram-merchant/pages/users/users.js
@@ -0,0 +1,18 @@
+// pages/users/users.js
+const { request } = require('../../utils/request.js');
+Page({
+  data: { list: [], total: 0, page: 1, pageSize: 20, status: '', keyword: '' },
+  onShow() { this.load(); },
+  onReachBottom() {
+    if (this.data.list.length < this.data.total) {
+      this.setData({ page: this.data.page + 1 }, () => this.load(true));
+    }
+  },
+  async load(append = false) {
+    const res = await request({
+      url: `/admin/users?page=${this.data.page}&pageSize=${this.data.pageSize}&status=${this.data.status}&keyword=${this.data.keyword}`
+    });
+    const list = append ? [...this.data.list, ...res.data.list] : res.data.list;
+    this.setData({ list, total: res.data.total });
+  }
+});
