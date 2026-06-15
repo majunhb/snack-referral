60e4c7215b466fec6b6deda44ac0b4be696e5bbe
majunhb <264765519@qq.com>
2026-06-15 18:24:49 +0800

    feat: add server/src/utils/logger.js

---
+++ server/src/utils/logger.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+const path = require('path');
+
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'snack-referral' },
+  transports: [
+    new winston.transports.File({ filename: path.join(__dirname, '../../logs/error.log'), level: 'error' }),
+    new winston.transports.File({ filename: path.join(__dirname, '../../logs/combined.log') }),
+    new winston.transports.Console({ format: winston.format.combine(winston.format.colorize(), winston.format.simple()) })
+  ]
+});
+
+module.exports = logger;
